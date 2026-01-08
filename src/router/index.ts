47708af9cfbc57d6f4dfd3eccd62ad3422aae5dd
lukas-hufnagl <lukas.hufnagl02@gmail.com>
2026-01-08 22:51:04 +0100

    Added cname file

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,7 +10,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/portfolio/'),
   routes,
   scrollBehavior(to) {
     if (to.hash) {
